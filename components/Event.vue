79079f7191a1289ec7aa26f6ef89932f4b15c8d8
jasenmichael <jasen@ready-builder.com>
2019-07-01 01:42:06 -0500

    implement pagination

--- components/Event.vue
+++ components/Event.vue
@@ -17,9 +17,16 @@
         <mdb-row>
           <mdb-card class="mx-auto" style="min-width:90%;">
             <mdb-card-body class="text-center">
-              <mdb-card-title v-if="event.summary.length <= 80">
-                {{ event.summary }}
-                <h1 class="text-hide">{{ event.name.text }}</h1>
+              <h2 v-if="event.summary.length <= 80">
+                <!-- {{ event.summary }} - -->
+                {{ getDate(event.start.local, 'dddd, MMMM Do YYYY') }}
+              </h2>
+              <mdb-card-title>
+                <h3>{{ event.summary }}</h3>
+                <!-- {{ getDate(event.start.local, 'dddd MMMM Do') }} -->
+                <h1 class="text-hide">
+                  {{ getDate(event.start.local, 'dddd MMMM Do') }}
+                </h1>
               </mdb-card-title>
               <h5 class="indigo-text">
                 <strong>
@@ -36,80 +43,37 @@
               <mdb-row>
                 <mdb-col sm="12" md="6">
                   <div>
-                    {{ event.status != 'completed' ? 'Begins' : 'Began' }}
+                    {{ event.status != 'completed' ? 'Starts' : 'Started' }}
                   </div>
-                  <div class="pb-2">At:</div>
+                  <!-- <div class="pb-2">At:</div> -->
+                  <!-- <mdb-icon far icon="clock" size="3x" /> -->
+                  <h1>{{ getDate(event.start.local, 'h:mma') }}</h1>
                 </mdb-col>
                 <mdb-col sm="12" md="6">
                   <div>
                     {{ event.status != 'completed' ? 'Ends' : 'Ended' }}
                   </div>
-                  <div class="pb-2">At:</div>
+                  <!-- <div class="pb-2">At:</div> -->
+                  <!-- <mdb-icon far icon="clock" size="3x" /> -->
+                  <h1>{{ getDate(event.end.local, 'h:mma') }}</h1>
                 </mdb-col>
+                <div
+                  class="content ml-4 mr-4 text-left"
+                  v-html="
+                    event.description.html
+                      .replace(event.summary, '')
+                      .replace(/<IMG/g, '<img')
+                      .replace(
+                        /<img/g,
+                        '<img class=\'img-fluid event-image z-depth-1-half\' align=\'left\''
+                      )
+                      .replace(
+                        /<iframe/g,
+                        '<div class=\'embed-responsive embed-responsive-16by9 z-depth-1-half\'><iframe class=\'embed-responsive-item\''
+                      )
+                  "
+                ></div>
               </mdb-row>
-              <!-- <mdb-card-text>
-
-              </mdb-card-text> -->
-              <!-- <mdb-btn
-                v-if="event.status !== 'completed'"
-                outine="primary"
-                class="register"
-                color="green"
-                tag="a"
-                target="_blank"
-                :href="event.url"
-                icon="leaf"
-              >
-                {{ event.is_free ? 'RSVP' : 'REGISTER' }}
-              </mdb-btn>
-              <br />
-              <hr />
-              <div>
-                <a class="px-2 fa-2x tw-ic" style="min-width=48px;">
-                  <mdb-icon fab class="blue-text tw-icon" icon="twitter" />
-                </a>
-                <a class="px-2 fa-2x fb-ic" style="min-width=64px;">
-                  <mdb-icon
-                    fab
-                    icon="facebook"
-                    class="indigo-text fb-icon"
-                    aria-hidden="true"
-                  />
-                </a>
-                <a class="px-2 fa-2x li-ic" style="min-width=48px;">
-                  <mdb-icon
-                    fab
-                    icon="linkedin"
-                    class="light-blue-text li-icon"
-                  />
-                </a>
-              </div> -->
-            </mdb-card-body>
-          </mdb-card>
-        </mdb-row>
-      </mdb-col>
-      <div
-        class="content ml-4 mr-4"
-        v-html="
-          event.description.html
-            .replace(event.summary, '')
-            .replace(/<IMG/g, '<img')
-            .replace(
-              /<img/g,
-              '<img class=\'img-fluid event-image z-depth-1-half\' align=\'left\''
-            )
-            .replace(
-              /<iframe/g,
-              '<div class=\'embed-responsive embed-responsive-16by9 z-depth-1-half\'><iframe class=\'embed-responsive-item\''
-            )
-        "
-      ></div>
-    </mdb-container>
-    <mdb-container class="">
-      <mdb-col md="12" class="mb-3 event-info">
-        <mdb-row>
-          <mdb-card class="mx-auto" style="min-width:90%;">
-            <mdb-card-body class="text-center">
               <mdb-btn
                 v-if="event.status !== 'completed'"
                 outine="primary"
@@ -175,13 +139,11 @@
         }
       ]"
     ></mdb-google-map>
-    <!-- <div>
-      <pre>{{ event }}</pre>
-    </div> -->
   </div>
 </template>
 
 <script>
+import moment from 'moment'
 import {
   mdbContainer,
   mdbGoogleMap,
@@ -189,7 +151,7 @@ import {
   mdbRow,
   mdbCard,
   mdbCardBody,
-  mdbCardTitle,
+  // mdbCardTitle,
   mdbIcon,
   mdbBreadcrumb,
   mdbBreadcrumbItem,
@@ -204,8 +166,6 @@ export default {
     mdbRow,
     mdbCard,
     mdbCardBody,
-    mdbCardTitle,
-    // mdbCardText,
     mdbIcon,
     mdbBreadcrumb,
     mdbBreadcrumbItem,
@@ -237,6 +197,11 @@ export default {
       // img.parentNode.classList.add('img-right')
       // }
     }
+  },
+  methods: {
+    getDate(date, format) {
+      return moment(date).format(format)
+    }
   }
 }
 </script>
@@ -252,7 +217,6 @@ export default {
 .tw-icon:hover {
   color: rgb(240, 240, 240) !important;
   border-radius: 20%;
-  /* margin: 2px; */
 }
 .li-icon:hover {
   background-color: #03a9f4 !important;
@@ -267,43 +231,33 @@ export default {
   float: left;
   width: 100%;
 }
+.map {
+  position: absolute;
+  margin-bottom: -24px;
+}
 .event-image {
   margin-top: 0.5rem;
   margin-bottom: 1rem;
   position: relative;
   /* max-width: 100vw; */
 }
-.map {
-  position: absolute;
-  margin-bottom: -24px;
-}
-
 @media only screen and (min-width: 750px) {
   .event-image {
-    /* max-width: 40vw; */
     clear: both;
-    /* float: none; */
     display: inline-block;
     max-height: 300px;
     max-width: 100vw;
     border-radius: 2px;
-    /* padding: 0.4rem; */
     margin-right: 1rem;
     margin-left: 0.5rem;
     margin-bottom: 1rem;
-    /* padding-top: 0.4rem; */
   }
-
   .img-right {
     float: none;
-    /* display: block; */
     display: inline-block;
   }
 }
 .content {
-  /* max-width: 100%; */
-  /* clear: both; */
-  /* display: block; */
   overflow: hidden;
 }
 </style>
