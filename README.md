0efac863f3794e3e03e2dd58cb12d61a68cbf01f
jasenmichael <jasen@ready-builder.com>
2019-07-07 21:22:24 -0500

    edit test function

--- README.md
+++ README.md
@@ -4,3 +4,18 @@
 uses: vue, nuxt, mdbootstrap, eventbright, and netlify
 
 based on https://github.com/jasenmichael/nuxt-eventbright-starter
+
+
+
+
+todo:
+ - implement Home School Enrichment Program page with schedule(google calendar)
+ - edit about to include more history(with old images), move home school section above and seperate from worksops/events
+ - fix events empty(refactor events), when not loaded through spa
+   - reconstruct(remove not needed fields) events list response stored in the store.
+   - remove events.list, and replace with event.live and events.completed.
+   - move axios.get to netlify functions
+
+ - move send email to netlify function
+   - add a .env key validation netify & email-endpiont
+   -  
--- functions/contactFormSend.js
+++ functions/contactFormSend.js
@@ -1,9 +1,62 @@
-exports.handler = function(event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    message: 'wazzuh',
-    body: event.body,
-    event,
-    context
-  })
+/* eslint-disable */
+
+// const axios = require('axios');
+
+exports.handler = function(event, context, callback){
+  // const { EMAIL_KEY } = process.env
+
+  // Send response back to client
+  const response = body => {
+    callback(null, {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json',
+        // 'Access-Control-Allow-Origin': 'localhost:9000',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+      },
+      body: JSON.stringify(body)
+    })
+  }
+
+  const errorResponse = (error, statusCode) => {
+    callback(null, {
+      unauthorized: 407,
+      statusCode: statusCode || unauthorized,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(error)
+    })
+  }
+
+  // validate
+
+  // hit api and get response
+  const sendEmail = () => {
+    // axios.get(URL)
+    //   .then(res => response(res.data))
+    //   .catch(err => response(err));
+    let reqBody = event.body == '' ? JSON.parse(event.body) : ''
+
+    // the body to send a msg back to the client
+    const responseBody = {
+      message: "yo",
+      context,
+      event,
+      reqBody   // : JSON.parse(event.body)
+    }
+    
+    response(responseBody)
+  }
+
+  // if(event.httpMethod == 'GET') {
+  if(event.httpMethod == 'POST') {
+    sendEmail()
+  } else {
+    error = {
+      error: true,
+      message: `'ERROR ${event.httpMethod} METHOD NOT ALLOWED'`
+    }
+    errorResponse(error, 405)
+  }
 }
