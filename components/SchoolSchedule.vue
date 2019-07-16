f47134ce8d7f4d503fc1212f034a80e17f24f7d4
jasenmichael <jasen@ready-builder.com>
2019-07-15 22:21:11 -0500

    implement read class schedule from api

--- components/SchoolSchedule.vue
+++ components/SchoolSchedule.vue
@@ -10,7 +10,7 @@
       <h4>{{ day }}s</h4>
       <mdb-datatable
         class="pt-4"
-        :data="{ columns: classes.columns, rows: getClassesOn(day) }"
+        :data="{ columns, rows: getClassesOn(day) }"
         responsive
         striped
         bordered
@@ -18,7 +18,7 @@
         :pagination="false"
         :sorting="false"
         :tfoot="false"
-        :maxHeight="'490px'"
+        :max-height="'490px'"
       />
       <hr
         v-if="`getDaysClassesHeld-${index}` !== getDaysClassesHeld().length"
@@ -26,8 +26,8 @@
     </div>
   </div>
 </template>
-
 <script>
+import axios from 'axios'
 import { mdbDatatable } from 'mdbvue'
 export default {
   name: 'DatatablePage',
@@ -37,146 +37,156 @@ export default {
   data() {
     return {
       daysClassesHeld: [],
-      classes: {
-        columns: [
-          {
-            label: 'Starts At',
-            field: 'starts_at'
-          },
-          {
-            label: 'Activity/Class',
-            field: 'class'
-          },
-          {
-            label: 'Age Range',
-            field: 'ages'
-          },
-          {
-            label: 'Teacher(s)',
-            field: 'teachers'
-          },
-          {
-            label: 'Ends At',
-            field: 'ends_at'
-          }
-        ],
-        rows: [
-          {
-            starts_at: '9:30 AM',
-            class: `Teacher's Lounge/Brunch Kid's Free Play`,
-            ages: 'all',
-            teachers: 'All',
-            ends_at: '10:00 AM',
-            daysClassHeld: ['Tuesday', 'Wednesday']
-          },
-          {
-            starts_at: '10:00 AM',
-            class: `Gardening/Nature`,
-            ages: 'all',
-            teachers: 'Dave/Rotating',
-            ends_at: '11:00 AM',
-            daysClassHeld: ['Tuesday']
-          },
-          {
-            starts_at: '11:00 AM',
-            class: `Math`,
-            ages: 'all',
-            teachers: 'Dave/Kerry',
-            ends_at: '12:00 AM',
-            daysClassHeld: ['Tuesday']
-          },
-          {
-            starts_at: '12:00 PM',
-            class: `Lunch`,
-            ages: 'all',
-            teachers: '',
-            ends_at: '1:00 PM',
-            daysClassHeld: ['Tuesday']
-          },
-          {
-            starts_at: '1:00 PM',
-            class: `Social Studies`,
-            ages: '10-17',
-            teachers: 'Dave',
-            ends_at: '1:30 PM',
-            daysClassHeld: ['Tuesday']
-          },
-          {
-            starts_at: '1:40 PM',
-            class: `Science`,
-            ages: '13-17',
-            teachers: 'Kerry',
-            ends_at: '3:00 PM',
-            daysClassHeld: ['Tuesday']
-          },
-          {
-            starts_at: '3:00 PM',
-            class: `Music`,
-            ages: '10-17',
-            teachers: 'Dave',
-            ends_at: '4:00 PM',
-            daysClassHeld: ['Tuesday']
-          },
-          {
-            starts_at: '4:00 PM',
-            class: `English`,
-            ages: '10-17',
-            teachers: 'Dave',
-            ends_at: '5:00 PM',
-            daysClassHeld: ['Tuesday']
-          },
-          {
-            starts_at: '10:00 AM',
-            class: `Math`,
-            ages: '7-17',
-            teachers: 'Dave/Kerry',
-            ends_at: '12:00 PM',
-            daysClassHeld: ['Wednesday']
-          },
-          {
-            starts_at: '12:00 PM',
-            class: `Lunch/Cooking`,
-            ages: '7-17',
-            teachers: '',
-            ends_at: '1:30 PM',
-            daysClassHeld: ['Wednesday']
-          },
-          {
-            starts_at: '1:40 PM',
-            class: `Science`,
-            ages: '8-17',
-            teachers: 'Kerry',
-            ends_at: '3:00 PM',
-            daysClassHeld: ['Wednesday']
-          },
-          {
-            starts_at: '3:00 PM',
-            class: `Art`,
-            ages: 'all',
-            teachers: 'Jojo',
-            ends_at: '4:00 PM',
-            daysClassHeld: ['Wednesday']
-          },
-          {
-            starts_at: '4:00 PM',
-            class: `English`,
-            ages: '10-17',
-            teachers: 'Dave',
-            ends_at: '5:00 PM',
-            daysClassHeld: ['Wednesday']
-          }
-        ]
-      }
+      // classes: {
+      columns: [
+        {
+          label: 'Starts At',
+          field: 'starts_at'
+        },
+        {
+          label: 'Activity/Class',
+          field: 'name'
+        },
+        {
+          label: 'Age Range',
+          field: 'ages'
+        },
+        {
+          label: 'Teacher(s)',
+          field: 'teachers'
+        },
+        {
+          label: 'Ends At',
+          field: 'ends_at'
+        }
+      ],
+      classes: []
+      // classes: [
+      //   {
+      //     starts_at: '9:30 AM',
+      //     name: `Teacher's Lounge/Brunch Kid's Free Play`,
+      //     ages: 'all',
+      //     teachers: 'All',
+      //     ends_at: '10:00 AM',
+      //     days: ['Tuesday', 'Wednesday']
+      //   },
+      //   {
+      //     starts_at: '10:00 AM',
+      //     name: `Gardening/Nature`,
+      //     ages: 'all',
+      //     teachers: 'Dave/Rotating',
+      //     ends_at: '11:00 AM',
+      //     days: ['Tuesday']
+      //   },
+      //   {
+      //     starts_at: '11:00 AM',
+      //     name: `Math`,
+      //     ages: 'all',
+      //     teachers: 'Dave/Kerry',
+      //     ends_at: '12:00 AM',
+      //     days: ['Tuesday']
+      //   },
+      //   {
+      //     starts_at: '12:00 PM',
+      //     name: `Lunch`,
+      //     ages: 'all',
+      //     teachers: '',
+      //     ends_at: '1:00 PM',
+      //     days: ['Tuesday']
+      //   },
+      //   {
+      //     starts_at: '1:00 PM',
+      //     name: `Social Studies`,
+      //     ages: '10-17',
+      //     teachers: 'Dave',
+      //     ends_at: '1:30 PM',
+      //     days: ['Tuesday']
+      //   },
+      //   {
+      //     starts_at: '1:40 PM',
+      //     name: `Science`,
+      //     ages: '13-17',
+      //     teachers: 'Kerry',
+      //     ends_at: '3:00 PM',
+      //     days: ['Tuesday']
+      //   },
+      //   {
+      //     starts_at: '3:00 PM',
+      //     name: `Music`,
+      //     ages: '10-17',
+      //     teachers: 'Dave',
+      //     ends_at: '4:00 PM',
+      //     days: ['Tuesday']
+      //   },
+      //   {
+      //     starts_at: '4:00 PM',
+      //     name: `English`,
+      //     ages: '10-17',
+      //     teachers: 'Dave',
+      //     ends_at: '5:00 PM',
+      //     days: ['Tuesday']
+      //   },
+      //   {
+      //     starts_at: '10:00 AM',
+      //     name: `Math`,
+      //     ages: '7-17',
+      //     teachers: 'Dave/Kerry',
+      //     ends_at: '12:00 PM',
+      //     days: ['Wednesday']
+      //   },
+      //   {
+      //     starts_at: '12:00 PM',
+      //     name: `Lunch/Cooking`,
+      //     ages: '7-17',
+      //     teachers: '',
+      //     ends_at: '1:30 PM',
+      //     days: ['Wednesday']
+      //   },
+      //   {
+      //     starts_at: '1:40 PM',
+      //     name: `Science`,
+      //     ages: '8-17',
+      //     teachers: 'Kerry',
+      //     ends_at: '3:00 PM',
+      //     days: ['Wednesday']
+      //   },
+      //   {
+      //     starts_at: '3:00 PM',
+      //     name: `Art`,
+      //     ages: 'all',
+      //     teachers: 'Jojo',
+      //     ends_at: '4:00 PM',
+      //     days: ['Wednesday']
+      //   },
+      //   {
+      //     starts_at: '4:00 PM',
+      //     name: `English`,
+      //     ages: '10-17',
+      //     teachers: 'Dave',
+      //     ends_at: '5:00 PM',
+      //     days: ['Wednesday', 'Friday']
+      //   }
+      // ]
+      // }
     }
   },
   beforeMount() {
     this.daysClassesHeld = this.getDaysClassesHeld
   },
+  mounted() {
+    return axios
+      .get('http://localhost:9000/.netlify/functions/classes')
+      .then(res => {
+        // eslint-disable-next-line
+        console.log('YOOOOOOOOOOO', res)
+        this.classes = res.data
+      })
+  },
   methods: {
     getDaysClassesHeld: function() {
       const daysClassesHeld = []
-      this.classes.rows.forEach(course => {
-        course.daysClassHeld.forEach(day => {
+      this.classes.forEach(course => {
+        course.days.forEach(day => {
           if (!daysClassesHeld.includes(day)) {
             daysClassesHeld.push(day)
           }
@@ -185,9 +195,7 @@ export default {
       return daysClassesHeld
     },
     getClassesOn: function(day) {
-      const classes = this.classes.rows.filter(el =>
-        el.daysClassHeld.includes(day)
-      )
+      const classes = this.classes.filter(el => el.days.includes(day))
       return classes.sort(this.sortByEarliest)
     },
     sortByEarliest: function(a, b) {
---
+++ functions/classes.js
@@ -0,0 +1,221 @@
+/* eslint-disable */
+const axios = require('axios')
+require('dotenv').config()
+
+const { STRAPI_USER, STRAPI_PASS, STRAPI_URL } = process.env
+const url = STRAPI_URL
+exports.handler = function(event, context, callback){
+
+  // Send response back to client
+  const response = body => {
+    callback(null, {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+      },
+      body: JSON.stringify(body)
+    })
+  }
+
+  // if an error send that..
+  const errorResponse = (error, statusCode) => {
+    callback(null, {
+      unauthorized: 407,
+      statusCode: statusCode || unauthorized,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(error)
+    })
+  }
+  
+  // authentication
+  const getToken = (user, pass) => {
+    axios.post(url + "/auth/local", {
+      // "identifier": STRAPI_USER,
+      // "password": STRAPI_PASS
+      "identifier": user || '',
+      "password": pass || ''
+    })
+      .then(res => response({token: res.data.jwt}))
+      .catch(error => response({error: "invalid credentials"}))
+  }
+
+  // crud route methods for - classes
+  // Create class
+  const createClass = (token, newClass) => {
+    console.log("class created", newClass)
+    const config = {
+      headers: {'Authorization': token}
+    }
+    axios.post(url + "/classes", newClass, config)
+      .then(res => response(res.data))
+      .catch(err => response(err))
+  }
+  // Read - list classes
+  const getClasses = () => {
+    axios.get(url + "/classes")
+      .then(res => {
+        let classes = res.data
+        console.log(classes)
+        let formattedClasses = []
+        for (let i = 0; i < classes.length; i++) {
+          const course = classes[i];
+          formattedClasses.push({
+            _id: course._id,
+            name: course.name,
+            starts_at: course.starts_at,
+            ages: course.ages || 'all',
+            teachers: getNamesArray(course.teachers).toString(),
+            ends_at: course.ends_at,
+            days: getNamesArray(course.days)
+          })
+        }
+        // response(res.data)
+        console.log(formattedClasses)
+        response(formattedClasses)
+      })
+      .catch(err => response(err))
+  }
+  // Read - get single class
+  const getClassById = id => {
+    axios.get(url + "/classes/" + id)
+      .then(res => response(res.data))
+      .catch(err => response(err))
+  }
+  // Update - update a gingle class
+  const updateClass = (token, id, updatedClass) => {
+    const config = {
+      headers: {'Authorization': token}
+    }
+    axios.put(url + "/classes/", updatedClass, config)
+    .then(res => response(res.data))
+    .catch(err => response(err))
+  }
+  // Delete - delete a single class
+  const deleteClass = (token, id) => {
+    const config = {
+      headers: {'Authorization': token}
+    }
+    axios.delete(url + "/classes/" + id, config)
+    .then(res => {
+      console.log(res.data)
+      response(res.data)
+    })
+    .catch(err => response(err))
+  }
+
+  // crud route methods for - teachers
+  // get list of teachers
+  const getTeachers = () => {
+    return axios.get(url + "/teachers")
+      .then(res => {
+        return res.data
+      })
+      .catch(err => console.log(err))
+  }  
+
+  // ------------------- helper functions ----------------
+  // get list of days, no need for crud on days
+  const getDays = () => {
+    return axios.get(url + "/days")
+      .then(res => {
+        return res.data
+      })
+      .catch(err => console.log(err))
+  }
+
+  // get object list names from object list id, returns an array of names(strings)
+  // used to reconstruct teachers and days object lists to array of names,
+  // and what we return to the client..item._id
+  const getNamesArray = objList => {
+    let names = objList.map(obj => obj.name )
+    return names
+  }
+  const getIdsArray = objList => {
+    let ids = objList.map(obj => obj._id )
+    return ids
+  }
+  // takes an array of string names, returns obj array with {_id:"value"}
+  // used to reconstruct array of string names to objects with _id for create/update
+  const getIdsObjArr = (namesArr, objList) => {
+    let results = objList.filter(item => namesArr.includes(item.name)).map(item => ({_id: item._id}))
+    console.log(results)
+    return results
+  }
+
+  ///////// TEST----------------------------
+  // getTeachers().then( teachers => {
+  //   // console.log(teachers)
+  //   getIdsObjArr(['Kerry', 'Laura', 'Dave'], teachers)
+  // }).catch(err => console.log(err))
+
+
+
+  // ---------------- ROUTES ----------------------------
+  // GET ------
+  if (event.httpMethod == 'GET') {
+    // Read get all classes --- /.netlify/functions/classes
+    if(!event.queryStringParameters.id && !event.queryStringParameters.login) {
+      getClasses()
+    }
+    // Read get individual class --- /.netlify/functions/classes/:id
+    if(event.queryStringParameters.id) {
+      getClassById(event.queryStringParameters.id)
+    }
+  }
+
+  // POST ------
+  if (event.httpMethod == 'POST') {
+    // Login ---  /.netlify/functions/classes?login
+    if (event.queryStringParameters.login == "" && event.body != '') {
+      const parsedBody = JSON.parse(event.body)
+      if (parsedBody.user && parsedBody.pass) {
+        getToken(parsedBody.user, parsedBody.pass)
+      } else {
+        let error = {
+          error: true,
+          message: `'Missing or invalid credentials'`
+        }
+      errorResponse(error, 405)
+      }
+    }
+    // Create a class ---  /.netlify/functions/classes?create
+    if(event.httpMethod == 'POST' && (event.queryStringParameters.create == "") && event.headers.authorization) {
+      const token = event.headers.authorization
+      createClass(token, JSON.parse(event.body))
+    }
+  }
+  // Update a class   /.netlify/functions/classes/:id
+  if((event.httpMethod == 'PUT') && event.queryStringParameters.id) {
+    const token = event.headers.authorization
+    const id = event.queryStringParameters.id
+    console.log("TOKEN: " + token, "\r\nID: " + id, "\r\nBODY: " + JSON.parse(event.body))
+    updateClass(token, id, JSON.parse(event.body))
+  }
+  // Delete a class ---  
+  if((event.httpMethod == 'DELETE') && event.queryStringParameters.id && event.headers.authorization) {
+    const token = event.headers.authorization
+    const id = event.queryStringParameters.id
+    console.log("DELETE", token, "\r\nID " + id)
+    deleteClass(token, id)
+  }
+
+
+
+
+  // dev log
+  // const log = () => {
+  //   let reqBody = event.body   // == '' ? JSON.parse(event.body) : ''
+  //   const info = {
+  //     message: "yo",
+  //     context,
+  //     event,
+  //     reqBody: reqBody != '' ? reqBody : '{}'    // : JSON.parse(event.body)
+  //   }
+  //   console.log(info)
+  // }
+  // log()
+}
